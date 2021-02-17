b11e7c0dab275c348a013c0692c3d134ada4d2f2
sarah-svg <kjskillen@gmail.com>
2021-02-17 14:28:38 -0800

    test one for CREATE_POST

--- src/reducers/postReducer.test.js
+++ src/reducers/postReducer.test.js
@@ -1,3 +1,21 @@
-describe('post reducer functionality', () => {
+import { createPost } from '../actions/postActions';
+import postReducer from './postReducers';
 
-})
+/* eslint-disable max-len */
+describe('post reducer functionality', () => {
+  it('checks to make sure the user is able to add a post with CREATE_POST', () => {
+    const state = {
+      post: []
+    };
+    const action = createPost({
+      post: 'hope it works'
+    });
+
+    const newState = postReducer(state, action);
+    expect(newState).toEqual({ post: [{  post: 'hope it works' }] });
+  });
+
+
+
+
+});
