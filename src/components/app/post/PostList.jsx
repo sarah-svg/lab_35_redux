6c285ecc4a0329b801473511f62df7c96245418c
sarah-svg <kjskillen@gmail.com>
2021-02-17 14:22:31 -0800

    testing

---
+++ src/components/app/post/PostList.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { getPost } from '../../../selectors/postSelectors';
+import { useSelector } from '../../../state/PostProvider';
+import Post from './Post';
+import uuid from 'react-uuid';
+
+const PostList = () => {
+
+  const posts = useSelector(getPost);
+
+  const postElements = posts.map(post => {
+    <li key={uuid()}>
+      <Post {...post}/>
+    </li>;
+  });
+  return (
+    <ul>
+      {postElements}
+    </ul>
+
+  );
+};
+export default PostList;
---
+++ src/reducers/postReducer.test.js
@@ -0,0 +1,3 @@
+describe('post reducer functionality', () => {
+
+})
