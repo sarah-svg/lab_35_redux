33f58eb23f297907ba5486eaae91cff43e1c4c76
sarah-svg <kjskillen@gmail.com>
2021-02-17 13:32:02 -0800

    postReducer takes in create and delete

--- src/reducers/postReducers.js
+++ src/reducers/postReducers.js
@@ -1,8 +1,28 @@
+import { CREATE_POST, DELETE_POST } from '../actions/postActions';
+
+
 export const intialState = {
   post: []
 };
 
 export default function postReducer(state, action) {
+  switch(action.type){
+    case CREATE_POST:
+      return {
+        ...state,
+        post: [...state.post, action.payload]
+      };
+    case DELETE_POST:
+      return {
+        ...state,
+        post: state.dogs.filter(post => post.post != action.payload)
+      };   
     default:
-        return state;
+      return state;
+  }
+    
+    
+    
+    
+
 }
