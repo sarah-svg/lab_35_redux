e5f25df29b30746cdd64db9aea43e6eb36fca1fa
sarah-svg <kjskillen@gmail.com>
2021-02-18 14:13:07 -0800

    comment component

---
+++ src/components/app/comment/comment.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { deleteComment } from '../../../actions/commentActions';
+
+const Comment = ({ comment, user }) => {
+  const  dispatch = useDispatch();
+  console.log(comment, user, 'commment');
+  const handleClick = () => {
+    dispatch(deleteComment(user));
+  };
+  return (
+    <div>
+      <span>comment: {comment}</span> 
+      <span>user: {user}</span> 
+      <button onClick={handleClick}>Delete Comment</button>
+    </div>
+  );
+};
+
+Comment.propTypes = {
+  comment: PropTypes.string.isRequired,
+  user: PropTypes.string.isRequired
+
+};
+
+export default Comment;
+
