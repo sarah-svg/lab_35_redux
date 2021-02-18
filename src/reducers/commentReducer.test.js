84ba25562ccca661e2cd55dd6daf1602f8b4df7b
sarah-svg <kjskillen@gmail.com>
2021-02-18 13:49:36 -0800

    tests function comment reducer to use a create comment and delete one

--- src/reducers/commentReducer.test.js
+++ src/reducers/commentReducer.test.js
@@ -1,4 +1,4 @@
-import { createCommit } from '../actions/commentActions';
+import { createCommit, deleteComment } from '../actions/commentActions';
 import commentReducer from './commentReducer';
 
 describe('comment reducer', () => {
@@ -11,5 +11,13 @@ describe('comment reducer', () => {
     });
     const newState = commentReducer(state, action);
     expect(newState).toEqual({ comment: [{ comment: 'hope its working' }] });
+  });
+
+  it('checks to make sure the user is able to delete a commit', () => {
+    const state = {
+      comment: [{ user: 'Dan' }]
+    };
+    const action = deleteComment('Dan');
+    expect(commentReducer(state, action)).toEqual({ comment: [] });
   });
 });
